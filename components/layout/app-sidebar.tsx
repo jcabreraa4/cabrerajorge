05accf219251da862e4b43a1553e1def863b6d36
Jorge Cabrera <jcabreraa4@proton.me>
2026-02-24 14:15:16 +0100

    Update sidebar UI

--- components/layout/app-sidebar.tsx
+++ components/layout/app-sidebar.tsx
@@ -2,40 +2,6 @@ import { NavMain } from '@/components/layout/nav-main';
 import { NavUser } from '@/components/layout/nav-user';
 import { Sidebar, SidebarHeader, SidebarContent, SidebarRail } from '@/components/ui/sidebar';
 import { currentUser } from '@clerk/nextjs/server';
-import { BotIcon, FileTextIcon, ImageIcon, LayoutDashboardIcon } from 'lucide-react';
-
-const sections = [
-  {
-    title: 'Dashboard',
-    items: [
-      {
-        title: 'Overview',
-        url: '/overview',
-        icon: LayoutDashboardIcon
-      },
-      {
-        title: 'Chatbots',
-        url: '/chatbots',
-        icon: BotIcon
-      }
-    ]
-  },
-  {
-    title: 'Storage',
-    items: [
-      {
-        title: 'Documents',
-        url: '/documents',
-        icon: FileTextIcon
-      },
-      {
-        title: 'Multimedia',
-        url: '/multimedia',
-        icon: ImageIcon
-      }
-    ]
-  }
-];
 
 export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const user = await currentUser();
@@ -55,7 +21,7 @@ export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sideb
         <NavUser user={userData} />
       </SidebarHeader>
       <SidebarContent>
-        <NavMain sections={sections} />
+        <NavMain />
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
--- components/layout/nav-main.tsx
+++ components/layout/nav-main.tsx
@@ -1,19 +1,53 @@
+'use client';
+
 import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type LucideIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { BotIcon, FileTextIcon, ImageIcon, LayoutDashboardIcon } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-interface NavMainProps {
-  sections: {
-    title: string;
-    items?: {
-      title: string;
-      url: string;
-      icon: LucideIcon;
-    }[];
-  }[];
-}
+const sections = [
+  {
+    title: 'Dashboard',
+    items: [
+      {
+        title: 'Overview',
+        url: '/overview',
+        icon: LayoutDashboardIcon
+      },
+      {
+        title: 'Chatbots',
+        url: '/chatbots',
+        icon: BotIcon
+      }
+    ]
+  },
+  {
+    title: 'Storage',
+    items: [
+      {
+        title: 'Documents',
+        url: '/documents',
+        icon: FileTextIcon
+      },
+      {
+        title: 'Multimedia',
+        url: '/multimedia',
+        icon: ImageIcon
+      }
+    ]
+  }
+];
 
-export function NavMain({ sections }: NavMainProps) {
+export function NavMain() {
+  const pathname = usePathname();
+  const segments = pathname.split('/').filter(Boolean);
+
+  function isActive(url: string) {
+    if (`/${segments[0]}` === url) return true;
+    return false;
+  }
+
   return (
     <>
       {sections.map((section, index) => (
@@ -26,6 +60,7 @@ export function NavMain({ sections }: NavMainProps) {
                   <SidebarMenuButton
                     asChild
                     tooltip={item.title}
+                    className={cn(isActive(item.url) && 'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground active:bg-primary active:text-primary-foreground')}
                   >
                     <Link href={item.url}>
                       <item.icon className="size-4" />
