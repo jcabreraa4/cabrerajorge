d3878a644a66b948f0e68cbe7f4ebb798d9b426d
Jorge Cabrera <jcabreraa4@proton.me>
2026-08-13 11:19:50 +0200

    Update

--- apps/private/app/(access)/layout.tsx
+++ apps/private/app/(access)/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className="h-full w-full p-5">{children}</main>;
+  return <main className="h-full w-full p-3 md:p-5">{children}</main>;
 }
